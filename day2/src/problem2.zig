6747b4a2d2f060f37e21184b88c7bf1306665327
D-Berg <danielfjberg@gmail.com>
2024-12-02 15:02:11 +0100

    day2: p2, program can tolerate bad level, gives wrong answer

--- day2/src/problem2.zig
+++ day2/src/problem2.zig
@@ -1,4 +1,6 @@
 const std = @import("std");
+const Allocator = std.mem.Allocator;
+const ArrayList = std.ArrayList;
 const print = std.debug.print;
 const log = std.log;
 
@@ -7,58 +9,126 @@ const Direction = enum {
     decreasing,
 };
 
-fn isSafe(line: []const u8) !bool {
+fn getOverallDirection(numbers: []const i32) Direction {
 
     var i: usize = 0;
-    var overall_dir: Direction = undefined;
+    var n_inc: u32 = 0;
+    var n_dec: u32 = 0;
 
-    var number_iterator = std.mem.split(u8, line, " ");
+    while (i < numbers.len - 1) : (i += 1) {
 
-    while (number_iterator.next()) |n1_str| : (i += 1) {
+        const dir = getDirection(numbers[i + 1] - numbers[i]);
 
-        const n1 = try std.fmt.parseInt(i32, n1_str, 10);
-        var n2: i32 = undefined;
-
-        if (number_iterator.peek()) |n2_str|{
-            n2 = try std.fmt.parseInt(i32, n2_str, 10);
-        } else {
-            break; // checked all pairs
+        switch (dir) {
+            .increasing => n_inc += 1,
+            .decreasing => n_dec += 1,
         }
+    }
 
+    if (n_inc > n_dec) {
+        return .increasing;
+    } else {
+        return .decreasing;
+    }
+}
 
-        const diff = n2 - n1;
-        if (diff == 0) {
-            log.debug("dir is stale", .{});
-            return false;
-        }
+/// issfirstTime: true if its the first time this function is called
+fn isSafe(numbers: []const i32, isFirstTime: bool, allocator: Allocator) !bool {
 
-        const current_dir = getDirection(diff);
+    var i: usize = 0;
+    var overall_dir: Direction = getOverallDirection(numbers);
 
-        log.debug("i = {} | n1 = {}, n2 = {}, diff = {}, dir = {s}", .{
-            i, n1, n2, diff, @tagName(current_dir)
-        });
+    if (isFirstTime) log.debug("\nchecking numbers = {any}, dir = {s}", .{numbers, @tagName(overall_dir)});
 
+    while (i < numbers.len - 1) : (i += 1) {
 
-        if (i == 0) {
-            overall_dir = current_dir;
-        } else {
+        const level_status = checkLevels(i, &overall_dir, numbers[i], numbers[i + 1]);
 
-            if (current_dir != overall_dir) {
-                log.debug("directions don't match for n1 = {}, n2 = {}, overall_dir = {s}, current_dir = {s}", .{
-                    n1, n2, @tagName(overall_dir), @tagName(current_dir)
-                });
-                return false;
+        if (level_status == .bad) {
+
+            if (isFirstTime) {
+
+                // Create subslice with first level removed
+                const slice_1 = try allocator.alloc(i32, numbers.len - 1);
+                defer allocator.free(slice_1);
+
+                var dest_idx: usize = 0;
+                for (numbers, 0..) |source_numb, source_idx| {
+                    if (source_idx == i) continue;
+                    slice_1[dest_idx] = source_numb; 
+                    dest_idx += 1;
+                } 
+
+                log.debug("checking sub_slice1 = {any}", .{slice_1});
+                if (try isSafe(slice_1, false, allocator)) {
+
+                    return true;
+
+                } else { // only check second subslice if 1st is unsafe
+                    
+                    // Create subslice with second level removed
+                    const slice_2 = try allocator.alloc(i32, numbers.len - 1);
+                    defer allocator.free(slice_2);
+
+                    dest_idx = 0;
+                    for (numbers, 0..) |source_numb, source_idx| {
+                        if (source_idx == i + 1) continue;
+                        slice_2[dest_idx] = source_numb; 
+                        dest_idx += 1;
+                    } 
+
+                    log.debug("checking sub_slice2 = {any}", .{slice_2});
+                    return try isSafe(slice_2, false, allocator);
+                }
             }
 
+            return false;
         }
-
-        const abs_diff = @abs(diff);
-        log.debug("abs diff = {}", .{abs_diff});
-        if (abs_diff < 1 or abs_diff > 3) return false;
 
     }
 
     return true;
+
+}
+
+const LevelStatus = enum {
+    ok,
+    bad
+};
+
+fn checkLevels(i: usize, overall_dir: *Direction, n1: i32, n2: i32) LevelStatus {
+
+    const diff = n2 - n1;
+    if (diff == 0) {
+        log.debug("dir is stale", .{});
+        return .bad;
+    }
+
+    const current_dir = getDirection(diff);
+
+    log.debug("i = {} | n1 = {}, n2 = {}, diff = {}, dir = {s}", .{
+        i, n1, n2, diff, @tagName(current_dir)
+    });
+
+
+    if (i == 0) {
+        overall_dir.* = current_dir;
+    } else {
+
+        if (current_dir != overall_dir.*) {
+            log.debug("directions don't match for n1 = {}, n2 = {}, overall_dir = {s}, current_dir = {s}", .{
+                n1, n2, @tagName(overall_dir.*), @tagName(current_dir)
+            });
+            return .bad;
+        }
+
+    }
+
+    const abs_diff = @abs(diff);
+    log.debug("abs diff = {}", .{abs_diff});
+    if (abs_diff < 1 or abs_diff > 3) return .bad;
+
+    return .ok;
 
 }
 
@@ -103,13 +173,25 @@ pub fn main() !void {
 
             if (line.len == 0) continue;
 
-            log.debug("line = {s}", .{line});
 
-            const is_safe = try isSafe(line);
+            var number_iterator = std.mem.split(u8, line, " ");
 
-            log.debug("is_safe = {}", .{is_safe});
+            var numbers = ArrayList(i32).init(allocator);
+            defer numbers.deinit();
 
-            if (is_safe) n_safe += 1;
+            // Gather numbers in an arraylist
+            while (number_iterator.next()) |n_str| {
+                try numbers.append(try std.fmt.parseInt(i32, n_str, 10));
+            }
+
+            const is_safe = try isSafe(numbers.items, true, allocator);
+
+            log.debug("numbers = {any}, is_safe = {}", .{numbers.items, is_safe});
+
+            if (is_safe) {
+
+                n_safe += 1;
+            }
 
         }
 
