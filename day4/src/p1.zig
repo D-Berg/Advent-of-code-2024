fc7ebe7d3c692c4d210178507e87202a204ec7a1
D-Berg <danielfjberg@gmail.com>
2024-12-04 10:31:05 +0100

    day4: p1 read input

--- day4/src/p1.zig
+++ day4/src/p1.zig
@@ -1,1 +1,54 @@
-pub fn main() !void {}
+const std = @import("std");
+const print = std.debug.print;
+const log = std.log;
+
+
+pub fn main() !void {
+
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+
+    const allocator = gpa.allocator();
+
+
+    const args = try std.process.argsAlloc(allocator);
+    defer std.process.argsFree(allocator, args);
+
+    if (args.len == 2) {
+
+        const file_name = args[1];
+
+
+        const file = try std.fs.cwd().openFile(file_name, .{});
+        defer file.close();
+
+        const file_stat = try file.stat();
+
+        const input = try file.readToEndAlloc(allocator, file_stat.size);
+        defer allocator.free(input);
+
+
+    }
+
+
+}
+
+
+test "example input" {
+
+    const input = 
+        \\MMMSXXMASM
+        \\MSAMXMSMSA
+        \\AMXSXMAAMM
+        \\MSAMASMSMX
+        \\XMASAMXAMM
+        \\XXAMMXXAMA
+        \\SMSMSASXSS
+        \\SAXAMASAAA
+        \\MAMMMXMMMM
+        \\MXMXAXMASX
+    ;
+
+    _ = input;
+
+}
