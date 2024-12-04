f330ae6546fe228b4aef29b31502be23b497c943
D-Berg <danielfjberg@gmail.com>
2024-12-04 11:15:50 +0100

    day4: p1 created a puzzle struct

--- day4/src/p1.zig
+++ day4/src/p1.zig
@@ -1,7 +1,53 @@
 const std = @import("std");
+const Allocator = std.mem.Allocator;
+const ArrayList = std.ArrayList;
 const print = std.debug.print;
 const log = std.log;
 
+const Puzzle = struct {
+    data: ArrayList(ArrayList(u8)),
+    n_rows: usize,
+    n_cols: usize,
+    
+    const Position = struct {
+        x: usize,
+        y: usize
+    };
+
+    fn init(allocator: Allocator, input: []const u8) !Puzzle {
+
+        var data = ArrayList(ArrayList(u8)).init(allocator);
+        
+        var iterator = std.mem.tokenizeAny(u8, input, "\n");
+
+        while (iterator.next()) |line| {
+            var row = ArrayList(u8).init(allocator);
+
+            for (line) |char| try row.append(char);
+
+            try data.append(row);
+        }
+
+        return Puzzle {
+            .data = data,
+            .n_rows = data.items.len,
+            .n_cols = data.items[0].items.len
+        };
+        
+    }
+
+    fn deinit(self: *const Puzzle) void {
+        for (self.data.items) |row| row.deinit();
+        self.data.deinit();
+    }
+
+    pub fn format(self: *const Puzzle, comptime fmt: []const u8, _: std.fmt.FormatOptions, writer: std.io.AnyWriter) !void {
+        if (fmt.len != 0) std.fmt.invalidFmtError(fmt, self);
+        for (self.data.items) |row| try writer.print("{s}\n", .{row.items});
+    }
+
+
+};
 
 pub fn main() !void {
 
@@ -27,6 +73,8 @@ pub fn main() !void {
         const input = try file.readToEndAlloc(allocator, file_stat.size);
         defer allocator.free(input);
 
+        const puzzle = try Puzzle.init(allocator, input);
+        defer puzzle.deinit();
 
     }
 
@@ -35,6 +83,7 @@ pub fn main() !void {
 
 
 test "example input" {
+    const allocator = std.testing.allocator;
 
     const input = 
         \\MMMSXXMASM
@@ -49,6 +98,9 @@ test "example input" {
         \\MXMXAXMASX
     ;
 
-    _ = input;
+    const puzzle = try Puzzle.init(allocator, input);
+    defer puzzle.deinit();
+
+    print("{}", .{puzzle});
 
 }
